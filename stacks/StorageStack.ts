bce1d25d51b1a80bf7e00b58720a5869dec2dd6a
JasonDL-trajector <emmanuel.delara@trajector.com>
2024-07-18 03:03:43 +0800

    Enabling CORS

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -1,7 +1,16 @@
 import { Bucket, StackContext, Table } from "sst/constructs";
 
 export function StorageStack({ stack }: StackContext) {
-  const bucket = new Bucket(stack, "SquirrelFundBucket");
+  const bucket = new Bucket(stack, "SquirrelFundBucket", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+        allowedHeaders: ["*"],
+      },
+    ],
+  });
   const table = new Table(stack, "SquirrelFundTable", {
     fields: {
       userId: 'string',
