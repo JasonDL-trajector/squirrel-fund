52b1bc9b43cf26b3202dbbb71f87418fd9beb8cb
Jason De Lara <142410817+JasonDL-trajector@users.noreply.github.com>
2024-07-25 04:43:40 +0800

    Footer

--- packages/frontend/src/components/Footer.tsx
+++ packages/frontend/src/components/Footer.tsx
@@ -1,28 +1,25 @@
-import React from 'react'
-import { Button } from "./ui/button"
-import { HomeIcon } from "./ui/HomeIcon"
-import { PieChartIcon } from "./ui/PiechartIcon"
-import { SettingsIcon } from "./ui/SettingsIcon"
+import React from "react";
+import { Button } from "./ui/button";
+import { HomeIcon } from "./ui/HomeIcon";
+import { PieChartIcon } from "./ui/PiechartIcon";
+import { SettingsIcon } from "./ui/SettingsIcon";
 
 const Footer = () => {
   return (
-    <footer className="bg-card border-t py-4 px-6 flex justify-between items-center mx-40">
-      <Button variant="ghost" size="icon" className="w-max px-5">
-        <HomeIcon className="w-6 h-6" label="Home" />
+    <footer className="bg-card border-t py-4 px-6 flex justify-between items-center">
+      <Button variant="ghost" size="icon" className="w-max">
+        <HomeIcon className="w-4 h-4 md:w-6 md:h-6" label="Home" />
       </Button>
 
-    
-        <Button variant="ghost" size="icon" className="w-max px-5">
-          <PieChartIcon className="w-6 h-6" />
-        </Button>
-    
+      <Button variant="ghost" size="icon" className="w-max">
+        <PieChartIcon className="w-4 h-4 md:w-6 md:h-6" />
+      </Button>
 
-      <Button variant="ghost" size="icon" className="w-max px-5">
-        <SettingsIcon className="w-6 h-6" />
+      <Button variant="ghost" size="icon" className="w-max">
+        <SettingsIcon className="w-4 h-4 md:w-6 md:h-6" />
       </Button>
     </footer>
-    
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
