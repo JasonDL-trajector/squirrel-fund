f6bdd27314549f50075e889090e84b01c6164403
JasonDL-trajector <emmanuel.delara@trajector.com>
2024-08-02 00:23:23 +0800

    API configure

--- packages/frontend/src/components/AmplifyProvider.tsx
+++ packages/frontend/src/components/AmplifyProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { ReactNode, useEffect } from 'react';
-import { Amplify } from 'aws-amplify';
+import { Amplify, API } from 'aws-amplify';
 import config from '../config';
 
 interface AmplifyProviderProps {
@@ -32,6 +32,31 @@ const AmplifyProvider = ({ children }: AmplifyProviderProps) => {
         ],
       },
     });
+
+    API.configure({
+      Auth: {
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        UserPoolId: config.cognito.USER_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        
+      },
+      Storage: {
+        region: config.s3.REGION,
+        bucket: config.s3.BUCKET,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      },
+      API: {
+        endpoints: [
+          {
+            name: "deposit",
+            endpoint: config.apiGateway.API_URL,
+            region: config.apiGateway.REGION,
+          },
+        ],
+      },
+    })
   }, []);
 
   return <>{children}</>;
